2e094149d5ad90d0c79048ad6774be1eef6d576c
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-09-06 17:59:42 +0300

    Implement a basic example plugin

---
+++ ExamplePlugin.cs
@@ -0,0 +1,24 @@
+using Basalt;
+using Basalt.Core.Plugins;
+
+[assembly: Plugin("ExamplePlugin", "1.0.0", Authors = ["BasaltBE"], EntryTypeName = "ExamplePlugin.ExamplePlugin")]
+
+namespace ExamplePlugin;
+
+public sealed class ExamplePlugin : Plugin
+{
+  public override void OnLoad()
+  {
+    Logger.Info($"{Description.Name} loaded.");
+  }
+
+  public override void OnStart()
+  {
+    Logger.Info($"{Description.Name} started.");
+  }
+
+  public override void OnDisable()
+  {
+    Logger.Info($"{Description.Name} disabled.");
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# ExamplePlugin
+
+Build and publish the plugin with:
+
+```powershell
+dotnet publish ExamplePlugin.csproj -c Release
+```
